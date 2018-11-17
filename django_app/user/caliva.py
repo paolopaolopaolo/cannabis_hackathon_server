e12db1b24e1c26ad8f09fd7e4b1f3f0bb0cd3785
emmanuel mawutor <emawutor@indiana.edu>
2018-11-17 14:36:30 -0800

    fixed api

--- django_app/user/caliva.py
+++ django_app/user/caliva.py
@@ -1,4 +1,5 @@
 import requests
+import pprtint
 
 base_url = "https://api.calivahack.io"
 products = "/products/v2/retail"
@@ -7,12 +8,14 @@ api_key = {'apikey': 'Caliva420'}
 search_location = "/locator/v2/storeLocator"
 locations = "/locator/v2/stores"
 
-class Caliva:
-  def search_for_products(id: None, name: None):
+
+class Caliva(object):
+
+  def search_for_products(id=None, name=None):
     # search for a single product by id
     product_id = f"/{id}"
     if product_id:
-      all_weed = requests.get(base_url+product_id,  headers= api_key)
+      all_weed = requests.get(base_url+products+product_id,  headers= api_key)
       for ganja in all_weed:
         return ganja
     else:
@@ -26,7 +29,7 @@ class Caliva:
         for weed in all_products:
           return weed
 
-  def search_for_location(location: None):
+  def search_for_location(location=None):
     # search for a single location
     if location:
       payload = { "location": str(location)}
