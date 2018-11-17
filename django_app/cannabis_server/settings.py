639ec0e9c5654e55a4145345c84dd93ce85e45b7
Dean Mercado <dean@yeti.co>
2018-11-17 14:36:36 -0800

    Blah

--- django_app/cannabis_server/settings.py
+++ django_app/cannabis_server/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
+    'django_seed',
+    'autofixture',
 
     # Local
     'user',
--- django_app/review/models.py
+++ django_app/review/models.py
@@ -1,14 +1,21 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
+from user.models import HighProfile
+
+
 class Review(models.Model):
-    user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(HighProfile, related_name='reviews', on_delete=models.CASCADE)
     product_id = models.IntegerField()
     body = models.TextField()
     date = models.DateTimeField(auto_now=True)
     rating = models.PositiveSmallIntegerField()
+
+    @property
+    def get_product(self):
+        ## TODO: Emmanual to insert all this
+        pass
 
 
 class Tag(models.Model):
